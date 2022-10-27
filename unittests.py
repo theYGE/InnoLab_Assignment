742936314243ddf98170ff752dc00af5f0daf1be
theYGE <alexandr.makarevych@gmail.com>
2022-10-27 10:11:52 +0200

    add another unittest for issue #1

--- unittests.py
+++ unittests.py
@@ -12,6 +12,11 @@ class TestKayaEquation(unittest.TestCase):
         expected = 1 * 2 * 3 * 10 / 3.67
         self.assertEqual(actual, expected)
 
+    def test_value_c_1(self):
+        actual = kaya_equation(1, 2, 30, 10, output_type="C")
+        expected = 1 * 2 * 30 * 10 / 3.67
+        self.assertEqual(actual, expected)
+
     def test_raise_error(self):
         with self.assertRaises(ValueError) as exception_context:
             kaya_equation(-1, 2, 3, 4)
